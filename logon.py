ca67b2b6c4d3acf3aa23862d50053e2094bb24e0
John David Ibañez <JhonhRge@gmail.com>
2019-05-24 09:01:15 -0500

    Update logon.py

--- logon.py
+++ logon.py
@@ -1,9 +1,7 @@
-
-import MySQLdb
+import pymysql
 import socket
 import datetime
 import getpass
-
 
 
 """
@@ -11,20 +9,24 @@ import getpass
       lines
       sudo apt install python3-dev libpython3-dev
       sudo apt install python3-mysqldb
+      sudo apt-get install python3-pymysql
 """
 def main():      
-      db = MySQLdb.connect(host="10.2.65.37",    # tu host, usualmente localhost
+      db = pymysql.connect(host="laboratorio.is.escuelaing.edu.co",    # tu host, usualmente localhost
                      user="control",         # tu usuario
                      passwd="control20101",  # tu password
                      db="control")
       cur = db.cursor();
       hostName = socket.gethostname()
+      #hostName = "CrkJohn-80"
       now = datetime.datetime.now()
       ip = "10.2.67."+ (hostName.split('-')[-1])
-      userLogin = getpass.getuser()
+      userLogin = getpass.getuser()      
       sql = "INSERT INTO datos(estudiante,equipo,logon,logoff,ip) VALUES(%s,%s,now(),null,%s)"
       val = (userLogin,hostName,ip)
       cur.execute(sql,val)
       db.commit()      
+      print(cur.rowcount, "record inserted.")
 
 main()
+
